fe7bdc29bfb5c0fbd92068aacb5204e9e02e0ef5
Triadnia <146646799+Triadnia@users.noreply.github.com>
2024-11-17 18:36:56 +0200

    Update_7(last) GildedRose.java
    Код повністю завершено. Додан метод для розрахунку  "Backstage passes", також виправлена помилка, де я забув написати ; після return.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -48,7 +48,7 @@ class GildedRose {
 
     private void updateItemQuality(Item item) {
         if(sulfurasB(item)) {
-            return
+            return;
         }
         decreaseSellIn(item);
         qualityType(item);
@@ -81,57 +81,19 @@ class GildedRose {
             increaseQuality(item);
         }
     }
-    
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < MaximumQuality) {
-                    items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < Backstage1) {
-                            if (items[i].quality < MaximumQuality) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < Backstage2) {
-                            if (items[i].quality < MaximumQuality) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < MaximumQuality) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+    private void updateBackPassQua(Item item) {
+        increaseQuality(item);
+        if (item.sellin < Backstage1) {
+            increaseQuality(item);
+        }
+        
+        if (item.sellin < Backstage2) {
+            increaseQuality(item);
+        }
+        
+        if (item.sellin < 0) {
+            item.quality = 0;
         }
     }
 }
